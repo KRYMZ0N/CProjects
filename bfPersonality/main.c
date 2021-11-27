986fb7961bd9092d9e0197ccf65d5a15fba4ffe5
KRYMZ0N <jackgrimes05@yahoo.com>
2021-11-27 01:38:13 -0500

    things

--- bfPersonality/main.c
+++ bfPersonality/main.c
@@ -8,29 +8,28 @@ int main()
 
     printf("Are you Gay???\n Yes or No!\n");
 
-    char myVariable[99];
+    char myVariable[99]; 
     scanf("%s", &myVariable);
     int i = 1;
 
-
     while (i > 0) {
-        if (strcmp("yes", myVariable) == 0) {
+        if (strcmp(myVariable, "yes\r") == 0) {
             printf("Cool\n");
             i = 0;
         } else {
 
-            if (strcmp("no", myVariable) == 0) {
+            if (strcmp(myVariable, "no") == 0) {
             printf("kys\n");
             i = 0;
             }
     
-            if (strcmp("no", myVariable) != 0) {
+            if (strcmp(myVariable, "no") != 0) {
             printf("That was not one of the options retard!\n");
             scanf("%s", &myVariable);
             }
         }
 
-        if (sizeof myVariable > 1) {
+        if (sizeof strtok(myVariable, " ") > 1) {
             printf("args are too long!\n");
             scanf("%s", &myVariable);
         }
